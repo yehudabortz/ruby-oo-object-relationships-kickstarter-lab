c34c12eb8b7f12185da799b55f87f66fc6be46e5
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-13 13:32:29 -0600

    commit tree 479d8d0ce94261f44fb9e0b0b2e936eec43b9d57

---
+++ lib/backer.rb
@@ -0,0 +1,25 @@
+class Backer 
+
+    attr_reader :name
+
+
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        project_backers = ProjectBacker.all.select do |pb|
+          pb.backer == self
+        end
+        project_backers.map do |pb|
+          pb.project
+        end
+      end
+
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,24 @@
+class Project
+    attr_reader :title
+
+
+    def initialize(title)
+        @title = title
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers
+        project_backers = ProjectBacker.all.select do |pb|
+          pb.project == self
+        end
+        project_backers.map do |pb|
+          pb.backer
+        end
+      end
+
+
+
+end
